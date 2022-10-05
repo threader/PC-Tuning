a8f667070ea222e0cff27cc1075a6052080124ca
AMIT <86924251+amitxv@users.noreply.github.com>
2022-10-05 20:19:12 +0100

    ControlSet001 -> CurrentControlSet

--- prerequisites/scripts/get-driver-keys.bat
+++ prerequisites/scripts/get-driver-keys.bat
@@ -11,7 +11,7 @@ if not !errorlevel! == 0 (
 
 for %%a in (Win32_VideoController, Win32_NetworkAdapter ) do (
     for /f "delims=" %%b in ('wmic path %%a get PnPDeviceID ^| findstr /l "PCI\VEN_"') do (
-        for /f "tokens=3" %%c in ('reg query "HKLM\SYSTEM\ControlSet001\Enum\%%b" /v "Driver"') do (
+        for /f "tokens=3" %%c in ('reg query "HKLM\SYSTEM\CurrentControlSet\Enum\%%b" /v "Driver"') do (
             for /f %%d in ('echo %%c ^| findstr "{"') do (
                 echo %%a : %%d
             )
