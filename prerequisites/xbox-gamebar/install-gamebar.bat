01c8066225d67453266218ab7dbb43b170ef9e48
AMIT <86924251+amitxv@users.noreply.github.com>
2022-10-10 18:07:56 +0100

    FIx current directory error in install-gamebar.bat

--- prerequisites/xbox-gamebar/install-gamebar.bat
+++ prerequisites/xbox-gamebar/install-gamebar.bat
@@ -6,13 +6,13 @@ dism > nul 2>&1 || echo error: administrator privileges required && pause && exi
 pushd "%~dp0"
 
 for %%a in (
-    "Microsoft.UI.Xaml.2.7_7.2208.15002.0_x64__8wekyb3d8bbwe.appx"
-    "Microsoft.VCLibs.140.00.UWPDesktop_14.0.30704.0_x64__8wekyb3d8bbwe.appx"
-    "Microsoft.VCLibs.140.00_14.0.30704.0_x64__8wekyb3d8bbwe.appx"
-    "Microsoft.XboxGamingOverlay_5.822.9161.0_neutral_~_8wekyb3d8bbwe.appxbundle"
+    ".\Microsoft.UI.Xaml.2.7_7.2208.15002.0_x64__8wekyb3d8bbwe.appx"
+    ".\Microsoft.VCLibs.140.00.UWPDesktop_14.0.30704.0_x64__8wekyb3d8bbwe.appx"
+    ".\Microsoft.VCLibs.140.00_14.0.30704.0_x64__8wekyb3d8bbwe.appx"
+    ".\Microsoft.XboxGamingOverlay_5.822.9161.0_neutral_~_8wekyb3d8bbwe.appxbundle"
 ) do (
-    if exist ".\%%a" (
-        PowerShell Add-AppxPackage -Path ".\%%a"
+    if exist %%a (
+        PowerShell Add-AppxPackage -Path %%a
     ) else (
         echo error: %%a not found
         exit /b 1
