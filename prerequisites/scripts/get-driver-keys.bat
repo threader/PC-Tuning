094c6ad0fa9365a49c313fc86dc6648b6686a234
AMIT <86924251+amitxv@users.noreply.github.com>
2022-11-15 18:34:42 +0000

    remove useless statement

--- prerequisites/scripts/get-driver-keys.bat
+++ prerequisites/scripts/get-driver-keys.bat
@@ -12,9 +12,7 @@ if not !errorlevel! == 0 (
 for %%a in (Win32_VideoController, Win32_NetworkAdapter ) do (
     for /f "delims=" %%b in ('wmic path %%a get PnPDeviceID ^| findstr /l "PCI\VEN_"') do (
         for /f "tokens=3" %%c in ('reg query "HKLM\SYSTEM\CurrentControlSet\Enum\%%b" /v "Driver"') do (
-            for /f %%d in ('echo %%c ^| findstr "{"') do (
-                echo %%a : %%d
-            )
+            echo %%a : %%c
         )
     )
     echo.
